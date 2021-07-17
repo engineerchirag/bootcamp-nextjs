6329f8dc6a107c38ba14acf47b5e608283dc11ce
Chirag Goel <chirag.goel@cleartax.in>
2021-07-18 00:41:46 +0530

    Lesson 3: Linking pages

---
+++ apps/nextjs-blog/pages/curriculum.js
@@ -0,0 +1,11 @@
+export default function Curriculum () {
+    return (
+        <>
+            <h2>Curriculum</h2>
+            <ul>
+                <li>Getting started with CNA app</li>
+                <li>Creating static pages</li>
+            </ul>
+        </>
+    )
+}
--- apps/nextjs-blog/pages/index.js
+++ apps/nextjs-blog/pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -18,9 +19,16 @@ export default function Home() {
         </p>
 
         <div className="grid">
+          <Link href="/curriculum">
+            <a className="card">
+              <h3>Documentation &rarr;</h3>
+              <p>Find in-depth information about Next.js features and API.</p>
+            </a>
+          </Link>
+
           <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h3>Curriculum &rarr;</h3>
+            <p>Find Next.js bootcamp detailed curriculum.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className="card">
